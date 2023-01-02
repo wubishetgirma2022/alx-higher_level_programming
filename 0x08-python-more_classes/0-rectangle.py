f2c8aa7d7e5fc26708e9e901a0065dda2a663070
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2023-01-01 23:32:01 -0800

    Create 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon jan 2 2023
+@author: wbishet girma
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
