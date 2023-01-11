c66f55bdbd819a2e41690d49c114234bbd33afb7
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2023-01-11 11:56:37 -0800

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: wbishet girma
+"""
+
+
 def is_same_class(obj, a_class):
-    """ Function that returns True/False if obj is a type of a_class
-    Args:
-        obj: object
-        a_class: class type
-    Returns:
-        True if type of obj is a_class
-        False, otherwise
     """
-    return type(obj) is 
+    Checks if two objects are the same class
+    """
+    if not isinstance(a_class, type):
+        raise TypeError("a_class must be a type")
+    return (type(obj) is a_class)
