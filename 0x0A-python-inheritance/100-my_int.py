07f792ca0f1bd93edbaa6b4c056c7aa32ba70fc4
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2023-01-11 11:39:56 -0800

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: wbishet girma
+"""
+
+
 class MyInt(int):
-    """ Class that inherits from class int"""
+    """
+    A class that inherits from int
+    """
+    def __eq__(self, num):
+        """
+        equal function for MyInt class
+        Attributes:
+            num (int): an inputed integer
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) != num)
 
-    def __eq__(self, other):
-        """ Method that returns != check """
-        return int.__ne__(self, other)
-
-    def __ne__(self, other):
-        """ Method that returns == check """
-        return int.__eq__(self, other)
+    def __ne__(self, num):
+        """
+        no equal function for MyInt class
+        Attributes:
+            num (int): an inputed integer
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) == num)
