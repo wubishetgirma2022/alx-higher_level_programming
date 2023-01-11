c1d9ea2a1621087c1d61ca0df7ba208f38488d95
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2023-01-11 11:57:40 -0800

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: wbishet girma
+"""
+
+
 def is_kind_of_class(obj, a_class):
-    """ Function that returns True/False if obj is an instance of a_class
-    Args:
-        obj: object
-        a_class: class type
-    Returns:
-        True if obj is an instance of a_class
-        False, otherwise
     """
-    return isinstance(obj, a_class)
+    Checks if object is an instance of class, or if the object is an instance\
+        of a class that inherited from
+    """
+    if not isinstance(a_class, type):
+        raise TypeError("a_class type must be type")
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
+        return True
+    return False
