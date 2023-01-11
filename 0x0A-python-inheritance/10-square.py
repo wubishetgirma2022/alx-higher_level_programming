d935f78b9660a2791142659dea8a733a39589fc8
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2023-01-11 11:45:22 -0800

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: wbishet girma
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Class that defines a Square from Rectangle class """
+    """
+    A Square class shape, inheirts from Rectangle and BaseGeometry
+    """
     def __init__(self, size):
-        """ Method that initializes a Square """
+        """"
+        Init function for Square
+        Attributes:
+            size (int): The size of the square
+        """
+        super().__init__(size, size)
         self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(self.__size, self.__size)
-
-    def area(self):
-        """ Method that returns a string with the area """
-        return super().area()
+        self.__size = 
