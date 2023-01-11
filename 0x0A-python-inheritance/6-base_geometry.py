8e4b6fd6111b0aed7980e97971ea3c8c6610ba67
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2023-01-11 11:50:53 -0800

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """ Empty class """
+# -*- coding: utf-8 -*-
+"""
+
+@author: wbishet girma
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
     def area(self):
+        """
+        Public instance method that calculates the area
+        Raises:
+            Exception if area is not imlemented
+        """
         raise Exception("area() is not implemented")
